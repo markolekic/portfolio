f24951dc22cf3a918e40c31bd48c44ba7c5240e4
Marko <61202605+markolekic@users.noreply.github.com>
2020-07-10 10:36:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
             $('.first-nav').addClass('nav-colorchange a');
             $('.first-nav').addClass('first-nav-down');
             $('.navbar-light').addClass('navbar-dark');
-            $('.animated-icon1').addClass('animated-icon1-white')
             $('nav').addClass('navbar-down');
             $('.logo').attr("src", "img/lekic-transparent1.png");
         } else {
@@ -16,7 +15,6 @@ $(document).ready(function() {
             $('.first-nav').removeClass('nav-colorchange a');
             $('.first-nav').removeClass('first-nav-down');
             $('.navbar-light').removeClass('navbar-dark');
-            $('.animated-icon1').removeClass('animated-icon1-white')
             $('nav').removeClass('navbar-down');
             $('.logo').attr("src", "img/lekic-transparent.png");
         }
